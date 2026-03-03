d4412ebc97045bea222022e2b5d85fa8bda41a86
Bignarem <35050327+Bignarem@users.noreply.github.com>
2026-03-03 16:28:05 -0400

    Add a second text field for the quantity

--- app/src/main/java/com/cnes/myshoppinglist/ShoppingList.kt
+++ app/src/main/java/com/cnes/myshoppinglist/ShoppingList.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -73,6 +72,14 @@ fun ShoppingListApp() {
                     OutlinedTextField(
                         value = itemName,
                         onValueChange = { itemName = it },
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    )
+                    OutlinedTextField(
+                        value = itemQuantity,
+                        onValueChange = { itemQuantity = it },
                         singleLine = true,
                         modifier = Modifier
                             .fillMaxWidth()

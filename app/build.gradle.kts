4646d84d0601f98f01a3cbd8fb52f668ad442711
Bignarem <35050327+Bignarem@users.noreply.github.com>
2026-03-20 13:23:08 -0400

    feat: Update navigation dependency to use navigation-compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,9 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("androidx.navigation:navigation-common-android:2.7.4")
+
+    // Navigation dependency
+    implementation("androidx.navigation:navigation-compose:2.7.4")
     // Location Services
     implementation("com.google.android.gms:play-services-location:21.3.0")
 

a5943c3109abc1f0e550ddb79c98f0bca0c51e72
Bignarem <35050327+Bignarem@users.noreply.github.com>
2026-03-20 15:54:27 -0400

    feat: Add GeocodingApiService interface for address retrieval from coordinates

---
+++ app/src/main/java/com/cnes/myshoppinglist/GeocodingApiService.kt
@@ -0,0 +1,13 @@
+package com.cnes.myshoppinglist
+
+import com.google.android.gms.maps.model.LatLng
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface GeocodingApiService {
+    @GET("maps/api/geocode/json")
+    suspend fun getAddressFromCoordinates(
+        @Query("latlng") latLng: String,
+        @Query("key") apiKey: String
+    ): GeoCodingResponse
+}
